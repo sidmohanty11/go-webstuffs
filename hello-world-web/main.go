be0695241f14fdcc2283daae5fcf1912536c8de4
Sidharth Mohanty <sidmohanty11@gmail.com>
2021-06-03 23:37:45 +0530

    vars and adding another about page

--- hello-world-web/main.go
+++ hello-world-web/main.go
@@ -6,15 +6,28 @@ import (
 	"net/http"
 )
 
+func Home(w http.ResponseWriter, r *http.Request) {
+	_, err := fmt.Fprintf(w, "Hello world")
+	if err != nil {
+		log.Fatalln(err.Error())
+	}
+}
+
+func About(w http.ResponseWriter, r *http.Request) {
+	sum := AddValues(2, 2)
+	_, err := fmt.Fprintf(w, fmt.Sprintf("The About Page and 2 + 2 is %d", sum))
+	if err != nil {
+		log.Fatalln(err.Error())
+	}
+}
+
+func AddValues(x, y int) int {
+	return x + y
+}
+
 func main() {
 	//url -> uniform resource locator!
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		n, err := fmt.Fprintf(w, "Hello world")
-		if err != nil {
-			log.Fatalln(err.Error())
-		}
-		fmt.Println(fmt.Sprintf("num of bytes written: %d", n))
-	})
-
+	http.HandleFunc("/", Home)
+	http.HandleFunc("/about", About)
 	http.ListenAndServe(":8000", nil)
 }
