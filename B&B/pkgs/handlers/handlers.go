9d34ada8b180a2f086a5265e3d1bed005a8630d9
Sidharth Mohanty <sidmohanty11@gmail.com>
2021-06-05 16:17:48 +0530

    making templates and connecting to router

--- B&B/pkgs/handlers/handlers.go
+++ B&B/pkgs/handlers/handlers.go
@@ -29,21 +29,35 @@ func NewHandlers(r *Repository) {
 
 //Home is the home page Handler
 func (m *Repository) Home(w http.ResponseWriter, r *http.Request) {
-	remoteIP := r.RemoteAddr
-	m.App.Session.Put(r.Context(), "remote_ip", remoteIP)
-
 	render.RenderTemplate(w, "home.page.html", &models.TemplateData{})
 }
 
 //About is the about page handler
 func (m *Repository) About(w http.ResponseWriter, r *http.Request) {
-	stringMap := make(map[string]string)
-	stringMap["test"] = "Passing string values from go."
+	render.RenderTemplate(w, "about.page.html", &models.TemplateData{})
+}
 
-	remoteIP := m.App.Session.GetString(r.Context(), "remote_ip")
-	stringMap["remote_ip"] = remoteIP
+//Generals is the Generals page handler
+func (m *Repository) Generals(w http.ResponseWriter, r *http.Request) {
+	render.RenderTemplate(w, "generals.page.html", &models.TemplateData{})
+}
 
-	render.RenderTemplate(w, "about.page.html", &models.TemplateData{
-		StringMap: stringMap,
-	})
+//Presidentals is the Presidentals page handler
+func (m *Repository) Presidentals(w http.ResponseWriter, r *http.Request) {
+	render.RenderTemplate(w, "presidentals.page.html", &models.TemplateData{})
+}
+
+//Reservation is the Reservation page handler
+func (m *Repository) Reservation(w http.ResponseWriter, r *http.Request) {
+	render.RenderTemplate(w, "make-reservation.page.html", &models.TemplateData{})
+}
+
+//About is the about page handler
+func (m *Repository) Availability(w http.ResponseWriter, r *http.Request) {
+	render.RenderTemplate(w, "search-availability.page.html", &models.TemplateData{})
+}
+
+//About is the about page handler
+func (m *Repository) Contact(w http.ResponseWriter, r *http.Request) {
+	render.RenderTemplate(w, "contact.page.html", &models.TemplateData{})
 }
--- B&B/routes.go
+++ B&B/routes.go
@@ -17,6 +17,11 @@ func routes(app *config.AppConfig) http.Handler {
 
 	mux.Get("/", handlers.Repo.Home)
 	mux.Get("/about", handlers.Repo.About)
+	mux.Get("/search-availability", handlers.Repo.Availability)
+	mux.Get("/make-reservation", handlers.Repo.Reservation)
+	mux.Get("/rooms/generals", handlers.Repo.Generals)
+	mux.Get("/rooms/presidentals", handlers.Repo.Presidentals)
+	mux.Get("/contact", handlers.Repo.Contact)
 
 	fileServer := http.FileServer(http.Dir("./static/")) //for serving static files
 	mux.Handle("/static/*", http.StripPrefix("/static", fileServer))
